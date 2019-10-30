2c4a27223044b382450726830c4ae0f904a08451
PianoFF <Fangfei1star@gmail.com>
2019-10-30 17:08:43 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+require "pry"
+class Dog
+    @@all = []
+attr_accessor :name
+    def initialize (name)
+        @name = name
+        save
+    end
+    
+    def self.all #class method
+        @@all
+    end  
+    # binding.pry
+    def self.clear_all
+        self.all.clear
+    end
+    def name
+        @name
+    end
+    def self.print_all
+        @@all.each{|dog| puts dog.name}
+    end
+    def save
+        @@all <<self 
+    end
+
+
+end
